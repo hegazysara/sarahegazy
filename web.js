20724cce383ff709fe1641af2f386b98d7743f3e
hegazysara <132402379+hegazysara@users.noreply.github.com>
2023-05-02 23:56:07 +0300

    Add files via upload

---
+++ web.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+function lipsticks(lipstick){
+    img.src = lipstick;
+}
+function colors(color){
+    container.style.background = color;
+    
+}
